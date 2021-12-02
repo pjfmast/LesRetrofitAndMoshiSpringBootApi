38da06624a4f0c4c095842f2d65793a68d55adeb
Paul de Mast <pjf.demast@avans.nl>
2021-12-02 18:24:20 +0100

    initial commit

---
+++ src/main/kotlin/nl/avans/avd/lesretrofitandmoshispringbootapi/Application.kt
@@ -0,0 +1,11 @@
+package nl.avans.avd.lesretrofitandmoshispringbootapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LesRetrofitAndMoshiSpringBootApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<LesRetrofitAndMoshiSpringBootApiApplication>(*args)
+}
---
+++ src/test/kotlin/nl/avans/avd/lesretrofitandmoshispringbootapi/LesRetrofitAndMoshiSpringBootApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.avans.avd.lesretrofitandmoshispringbootapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LesRetrofitAndMoshiSpringBootApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
